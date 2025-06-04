d36c1247bf7224f76911a1a2cab216b2b60e49b1
kirdhsali <59085302+kirdhsali@users.noreply.github.com>
2025-06-04 20:37:07 +0200

    Initial project structure for SNPedia trait extractor

---
+++ README.md
@@ -0,0 +1,3 @@
+# SNPedia Trait Extractor
+
+Tool to extract high-confidence SNP-phenotype associations from SNPedia.
---
+++ src/main.py
@@ -0,0 +1,7 @@
+# Entry point script
+
+def main():
+    print('Hello from SNPedia Trait Extractor')
+
+if __name__ == '__main__':
+    main()
